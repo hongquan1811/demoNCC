b2c9060070858acad2bd86f08fc9fd67c325d3d8
hongquan1811 <daohongquan1811@gmail.com>
2023-04-02 22:58:16 +0700

    second commit

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -23,6 +23,14 @@
                     </div>
                 </div>
                 <div class="input-group mb-3">
+                    <input type="email" name="email" class="form-control" placeholder="Email">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-envelope"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="input-group mb-3">
                     <input type="password" name="password" class="form-control" placeholder="Password">
                     <div class="input-group-append">
                         <div class="input-group-text">
